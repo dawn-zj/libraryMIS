f07f10d32bb00d972e2f0c37b8eafcc2b61d47f9
zhangjuan <867096367@qq.com>
2020-06-23 11:25:56 +0800

    遵循阿里规约，去除public abstract修饰符，添加javadoc注释

--- libraryMIS/src/cn/com/gs/ssm/libraryMIS/service/IBookService.java
+++ libraryMIS/src/cn/com/gs/ssm/libraryMIS/service/IBookService.java
@@ -4,14 +4,57 @@ import cn.com.gs.ssm.libraryMIS.model.Book;
 import cn.com.gs.ssm.libraryMIS.util.Page;
 
 public interface IBookService {
-	public abstract Page<Book> searchBookByPage(Page<Book> page,Book bk);
-	public abstract Integer searchTotal(Book bk);
-	public abstract Book selectBookById(String id);
-	
-	public abstract void deleteBook(String bookId);
-	public abstract Integer batchDeleteBooks(String bookIds);
-	
-	public abstract void insertBook(Book bk);
-	
-	public abstract void updateBook(Book bk);
+
+	/**
+	 * 增
+	 *
+	 * @param bk
+	 */
+	void insertBook(Book bk);
+
+	/**
+	 * 删除
+	 * @param bookId
+	 */
+	void deleteBook(String bookId);
+
+	/**
+	 * 批量删除
+	 * @param bookIds
+	 * @return
+	 */
+	Integer batchDeleteBooks(String bookIds);
+
+	/**
+	 * 改
+	 *
+	 * @param bk
+	 */
+	void updateBook(Book bk);
+
+	/**
+	 * 根据id查询
+	 *
+	 * @param id
+	 * @return
+	 */
+	Book selectBookById(String id);
+
+	/**
+	 * 分页查询图书
+	 *
+	 * @param page
+	 * @param bk
+	 * @return
+	 */
+	Page<Book> searchBookByPage(Page<Book> page,Book bk);
+
+	/**
+	 * 查询总记录
+	 *
+	 * @param bk
+	 * @return
+	 */
+	Integer searchTotal(Book bk);
+
 }
