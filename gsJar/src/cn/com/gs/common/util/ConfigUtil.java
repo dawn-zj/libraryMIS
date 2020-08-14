59fb9abff942ba263815c1842cda6443dcbc9ce3
zhangjuan <867096367@qq.com>
2020-08-14 17:06:57 +0800

    系统监控

--- gsJar/src/cn/com/gs/common/util/ConfigUtil.java
+++ gsJar/src/cn/com/gs/common/util/ConfigUtil.java
@@ -9,12 +9,26 @@ import org.apache.commons.configuration2.PropertiesConfiguration;
 import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
 import org.apache.commons.configuration2.builder.fluent.Parameters;
 import org.apache.commons.configuration2.convert.DefaultListDelimiterHandler;
+import org.apache.commons.lang3.StringUtils;
 
+import java.util.List;
+
+/**
+ * 配置类
+ */
 public class ConfigUtil {
+	/**
+	 * 实例
+	 */
 	private static ConfigUtil instance;
 	private Configuration properties;
 	private FileBasedConfigurationBuilder<FileBasedConfiguration> builder;
 
+	/**
+	 * 获取ConfigUtil实例
+	 *
+	 * @return
+	 */
 	public static synchronized ConfigUtil getInstance() {
 		if (instance == null) {
 			ConfigUtil config = new ConfigUtil();
@@ -29,7 +43,9 @@ public class ConfigUtil {
 		return instance;
 	}
 
-
+	/**
+	 * 加载配置文件
+	 */
 	private synchronized void load() {
 		try {
 			// 读取属性文件 .properties
@@ -42,6 +58,9 @@ public class ConfigUtil {
 		}
 	}
 
+	/**
+	 * 重新加载配置文件
+	 */
 	public synchronized void reload() {
 		try {
 			instance.load();
@@ -50,8 +69,53 @@ public class ConfigUtil {
 		}
 	}
 
+	/**
+	 * 超时时间
+	 *
+	 * @return
+	 */
 	public Long getTimeoutTime() {
 		return properties.getLong("timeout.time", 1800000L);
+	}
+
+	/**
+	 * 日志文件配置
+	 *
+	 * @return
+	 */
+	public String getLogToFile() {
+		List<Object> list = properties.getList("log.toFile");
+		String logToFile = StringUtils.join(list.toArray(), Constants.SPLIT_1);
+		return logToFile;
+	}
+
+	/**
+	 * 日志数据库配置
+	 *
+	 * @return
+	 */
+	public String getLogToDB() {
+		List<Object> list = properties.getList("log.toDB");
+		String logToDB = StringUtils.join(list.toArray(), Constants.SPLIT_1);
+		return logToDB;
+	}
+
+	/**
+	 * 日志文件大小
+	 *
+	 * @return
+	 */
+	public String getLogFileSize() {
+		return properties.getString("log.file_size", Constants.DEFAULT_STRING);
+	}
+
+	/**
+	 * 日志文件个数
+	 *
+	 * @return
+	 */
+	public String getLogFileNum() {
+		return properties.getString("log.file_num", Constants.DEFAULT_STRING);
 	}
 
 	public static void main(String[] args) {
--- gsJar/src/cn/com/gs/common/util/logger/LoggerConfig.java
+++ gsJar/src/cn/com/gs/common/util/logger/LoggerConfig.java
@@ -1,6 +1,10 @@
 package cn.com.gs.common.util.logger;
 
 import cn.com.gs.common.define.Constants;
+import cn.com.gs.common.util.ConfigUtil;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LoggerConfig {
 	/**
@@ -8,9 +12,64 @@ public class LoggerConfig {
 	 */
 	private static String logPath = Constants.LOG_PATH;
 
+	/**
+	 * 写入日志文件配置
+	 */
+	private static List<String> toFileList;
+
+	/**
+	 * 写入数据库配置
+	 */
+	private static List<String> toDBList;
+
+	/**
+	 * 文件大小
+	 */
+	private static int fileSize;
+
+	/**
+	 * 文件数量
+	 */
+	private static int fileNumber;
+
 
 	public static String getLogPath() {
 		return logPath;
+	}
+
+	/**
+	 * 初始化日志参数，系统启动时初始化一次即可
+	 */
+	public synchronized static void init() {
+		toFileList = new ArrayList<String>();
+		toDBList = new ArrayList<String>();
+
+		ConfigUtil config = ConfigUtil.getInstance();
+		String toFile = config.getLogToFile();
+		String toDB = config.getLogToDB();
+
+		String[] toFiles = toFile.split(",");
+		for (String string : toFiles)
+			toFileList.add(string);
+
+		String[] toDBs = toDB.split(",");
+		for (String string : toDBs)
+			toDBList.add(string);
+
+
+		fileSize = Integer.parseInt(ConfigUtil.getInstance().getLogFileSize());
+		fileNumber = Integer.parseInt(ConfigUtil.getInstance().getLogFileNum());
+
+	}
+
+	/**
+	 * 日志文件配置是否包含指定日志类型
+	 *
+	 * @param logMode 日志类型：错误日志、调试日志等等
+	 * @return boolean
+	 */
+	public synchronized static boolean fileIsContains(String logMode) {
+		return toFileList.contains(logMode);
 	}
 
 }
