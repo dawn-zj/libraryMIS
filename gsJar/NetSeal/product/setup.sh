dd737b7ab1541427e7d9b24f4564d3d2631bd6cc
zhangjuan <867096367@qq.com>
2020-08-25 10:31:06 +0800

    系统监控

--- gsJar/NetSeal/product/setup.sh
+++ gsJar/NetSeal/product/setup.sh
@@ -18,14 +18,18 @@ echo "[backup] [${version}] [${dateinfo}]">>updatelog.txt
 echo "setup copy files..."
 
 echo "cp files..."
+# 删除appserver原来的jar，放入新的jar
 rm -rf ${netsealhome}/appserver/lib
 cp -r ${currentpath}/lib ${netsealhome}/appserver
+# 删除Tomcat下部署的webserver，放入新的webserver
 rm -rf ${netsealhome}/tomcat/webapps/webserver
 cp -r ${currentpath}/webserver ${netsealhome}/tomcat/webapps
 
+# 备份watchdog的配置到conf下，删除watchdog
 cp -r ${netsealhome}/watchdog/conf/config.txt ${netsealhome}/conf/
 rm -rf ${netsealhome}/watchdog
 
+# 安装新的watchdog，并使用原来的watchdog配置
 sh ${currentpath}/watchdog/setup.sh
 
 mv ${netsealhome}/conf/config.txt ${netsealhome}/watchdog/conf/
